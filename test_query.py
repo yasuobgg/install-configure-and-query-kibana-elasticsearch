15f5da538681055d4a01aac1872a6393a89391b1
yasuobgg <yasuobgg@gmail.com>
2023-05-23 11:08:42 +0700

    first commit

---
+++ test_query.py
@@ -0,0 +1,29 @@
+from elasticsearch import Elasticsearch
+
+es = Elasticsearch("http://10.14.170.43:9200")
+# es.info().body
+
+resp = es.search(
+  _source =  ["genre","ethnicity","year",'cast'],  # set the fields that want to get
+  
+  index= "movies",  # set index
+  
+  size = 50,  # set the size to display
+  
+  # set the query by each want to search, find from kibana dev tools
+  query={
+      "query_string": {
+      "query": "(drama romance) AND (american tamil) AND (year:(>=2010)) AND (james)",
+      "fields": [
+        "genre",
+        "ethnicity",
+        "cast"
+      ]
+    }
+  }          
+)
+
+print("Got %d Hits:" % resp['hits']['total']['value'])
+
+for hit in resp['hits']['hits']:
+  print(hit["_source"])
